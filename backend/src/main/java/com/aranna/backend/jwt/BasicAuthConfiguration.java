982395a1a542df168dff2182f8101dd58aed8011
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-03-17 11:42:52 +0600

    upd:Set up actuator

---
+++ backend/src/main/java/com/aranna/backend/jwt/BasicAuthConfiguration.java
@@ -0,0 +1,21 @@
+package com.aranna.backend.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthConfiguration {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        return httpSecurity.authorizeHttpRequests(auth->
+            auth.requestMatchers("/**")
+            .permitAll()
+        )
+        .csrf(customizer->customizer.disable())
+        .httpBasic(customizer->customizer.disable())
+        .formLogin(customizer->customizer.disable())
+        .build();
+    }
+}
