fbffe8ae25941293442de71b8f29403cec94dedc
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-03-19 21:25:42 +0600

    upd:accounts microservice

--- microservices/accounts/src/main/java/com/accounts/accounts/controller/AccountsController.java
+++ microservices/accounts/src/main/java/com/accounts/accounts/controller/AccountsController.java
@@ -1,10 +1,8 @@
 package com.accounts.accounts.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.micrometer.observation.autoconfigure.ObservationProperties.Http;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
--- microservices/accounts/src/main/java/com/accounts/accounts/entity/Accounts.java
+++ microservices/accounts/src/main/java/com/accounts/accounts/entity/Accounts.java
@@ -2,8 +2,7 @@ package com.accounts.accounts.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
