6e921e615556a40ff9293f1b0721517a683c2e46
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-03-20 08:39:30 +0600

    upd:Readme.md && add:CustomGatewayRoutesConfig

--- microservices/gatewayserver/src/main/java/com/gatewayserver/gatewayserver/configuration/CustomRouteLocator.java
+++ microservices/gatewayserver/src/main/java/com/gatewayserver/gatewayserver/configuration/CustomRouteLocator.java
@@ -1,5 +1,7 @@
 package com.gatewayserver.gatewayserver.configuration;
 
+import java.time.LocalDateTime;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +13,15 @@ public class CustomRouteLocator {
     RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(p -> p.path("/hello/ac/**")
-                        .filters(f -> f.rewritePath("/hello/accounts/(?<segment>.*)", "/${segment}"))
-                        .uri("lb://ACCOUNTS"))
+                        .filters(f -> f.rewritePath("/hello/accounts/(?<segment>.*)", "/${segment}")
+                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                )
+                        .uri("lb://ACCOUNTS")
+                )
                 .route(p -> p.path("/hello/loans/**")
-                        .filters(f -> f.rewritePath("/hello/loans/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/hello/loans/(?<segment>.*)", "/${segment}")
+                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                )
                         .uri("lb://LOANS"))
                 .build();
     }
